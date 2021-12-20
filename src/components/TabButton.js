8ea5298a71ff93d2fa62f714f584cc8d3fd010bd
Vovchick4 <vovaspodpivas@gmail.com>
2021-12-20 22:36:33 +0200

    add anim

--- src/components/TabButton.js
+++ src/components/TabButton.js
@@ -1,25 +1,27 @@
 import React, { useEffect } from "react"
-import { Animated } from "react-native"
+import { View, Animated } from "react-native"
 import { Icon } from "react-native-elements"
 
 export default function TabButton({ typeIcon, icon, focused, ...props }) {
     const AnimationState = {
-        fromScale: new Animated.Value(1),
+        fromScale: new Animated.Value(1.2),
         tocale: new Animated.Value(2)
     }
 
     useEffect(() => {
         Animated.timing(AnimationState.fromScale, { toValue: 1.4, duration: 343, useNativeDriver: true }).start()
-        Animated.timing(AnimationState.tocale, { toValue: 1, duration: 438, useNativeDriver: true }).start()
+        Animated.timing(AnimationState.tocale, { toValue: 1.2, duration: 438, useNativeDriver: true }).start()
     })
 
     return (
-        <Animated.View style={focused ? { transform: [{ scale: AnimationState.fromScale }] } : { transform: [{ scale: 1 }] }}>
-            <Icon
-                name={icon}
-                type={typeIcon}
-                {...props}
-            />
-        </Animated.View>
+        <View>
+            <Animated.View style={focused ? { transform: [{ scale: AnimationState.fromScale }] } : { transform: [{ scale: 1.2 }] }}>
+                <Icon
+                    name={icon}
+                    type={typeIcon}
+                    {...props}
+                />
+            </Animated.View>
+        </View>
     )
 }
--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,52 +1,117 @@
-import React from "react";
-import { Text } from "react-native-paper";
+import React, { useRef } from "react";
+import 'react-native-gesture-handler'
+import { Animated, Easing, TouchableOpacity, Dimensions } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Icon } from "react-native-elements";
 
 import { MainStackNavigator, LoyaltyStackNavigator, RentStackNavigator } from "./StackNavigator";
 import { TabButton } from '../components';
 
 const Tab = createBottomTabNavigator();
 
-const config = {
+const openConfig = {
     animation: 'spring',
     config: {
-        stiffness: 1000,
-        damping: 5000,
-        mass: 3,
-        overshootClamping: true,
-        restDisplacementThreshold: 0.11,
-        restSpeedThreshold: 0.21,
+        duration: 434,
+        easing: Easing.linear
     },
 };
 
+const closeConfig = {
+    animation: 'spring',
+    config: {
+        duration: 434,
+        easing: Easing.linear
+    },
+};
+
+function getWidth() {
+    let width = Dimensions.get("window").width
+
+    // Horizontal Padding = 20...
+    width = width - 80
+
+    // Total five Tabs...
+    return width / 3
+}
+
 const BottomTabNavigator = () => {
+    // Animated Tab Indicator...
+    const tabOffsetValue = useRef(new Animated.Value(0)).current;
     return (
-        <Tab.Navigator screenOptions={{
-            headerShown: false,
-            tabBarLabelPosition: 'left',
-            tabBarStyle: { margin: 15, paddingBottom: 2, borderRadius: 10, backgroundColor: 'rgb(220, 38, 38)' },
-            // tabBarActiveBackgroundColor: 'gray',
-            tabBarActiveTintColor: 'white',
-            tabBarInactiveTintColor: 'black',
-        }}>
-            <Tab.Screen name="Emargency Tab" component={MainStackNavigator} options={{
-                tabBarLabel: 'Emargency',
-                tabBarIcon: (props) => <TabButton typeIcon="font-awesome-5" icon="phone-square-alt" {...props} />,
-                // tabBarVisibilityAnimationConfig: {
-                //     open: config,
-                //     close: config,
-                // },
-            }} />
-            <Tab.Screen name="Rent Tab" component={RentStackNavigator} options={{
-                tabBarLabel: 'Rent a car',
-                tabBarIcon: (props) => <TabButton typeIcon="ionicon" icon="ios-car" {...props} />,
-            }} />
-            <Tab.Screen name="Loyalty Tab" component={LoyaltyStackNavigator} options={{
-                tabBarLabel: 'Loyalty',
-                tabBarIcon: (props) => <TabButton typeIcon="ionicon" icon="ios-card" {...props} />,
-            }} />
-        </Tab.Navigator >
+        <React.Fragment>
+            <Tab.Navigator screenOptions={{
+                headerShown: false,
+                tabBarShowLabel: false,
+                // tabBarLabelPosition: 'beside-icon',
+                tabBarStyle: {
+                    position: 'absolute',
+                    bottom: 40,
+                    marginHorizontal: 20,
+                    height: 60,
+                    borderRadius: 10,
+                    shadowColor: '#ffff',
+                    shadowOpacity: 0.83,
+                    shadowOffset: {
+                        width: 4,
+                        height: 4,
+                    },
+                    paddingHorizontal: 20,
+                    backgroundColor: 'rgb(220, 38, 38)'
+                },
+                tabBarActiveTintColor: 'white',
+                tabBarInactiveTintColor: 'black',
+            }}>
+                <Tab.Screen name="Emargency Tab" component={MainStackNavigator} options={{
+                    tabBarIcon: (props) => <TabButton typeIcon="font-awesome-5" icon="phone-square-alt" {...props} />
+                }}
+                    listeners={({ navigation, route }) => ({
+                        // Onpress Update....
+                        tabPress: () => {
+                            Animated.spring(tabOffsetValue, {
+                                toValue: 0,
+                                useNativeDriver: true
+                            }).start();
+                        }
+                    })} />
+                <Tab.Screen name="Rent Tab" component={RentStackNavigator} options={{
+                    tabBarIcon: (props) => <TabButton typeIcon="ionicon" icon="ios-car" {...props} />,
+                }} listeners={({ navigation, route }) => ({
+                    // Onpress Update....
+                    tabPress: () => {
+                        Animated.spring(tabOffsetValue, {
+                            toValue: getWidth(),
+                            useNativeDriver: true
+                        }).start();
+                    }
+                })} />
+                <Tab.Screen name="Loyalty Tab" component={LoyaltyStackNavigator} options={{
+                    tabBarIcon: (props) => <TabButton typeIcon="ionicon" icon="ios-card" {...props} />,
+                }} listeners={({ navigation, route }) => ({
+                    // Onpress Update....
+                    tabPress: () => {
+                        Animated.spring(tabOffsetValue, {
+                            toValue: getWidth() * 2,
+                            useNativeDriver: true
+                        }).start();
+                    }
+                })} />
+            </Tab.Navigator>
+
+            <Animated.View style={{
+                width: getWidth() - 20,
+                height: 2,
+                backgroundColor: 'white',
+                position: 'absolute',
+                bottom: 100,
+                // Horizontal Padding = 20...
+                left: 50,
+                borderRadius: 20,
+                transform: [
+                    { translateX: tabOffsetValue }
+                ]
+            }}></Animated.View>
+        </React.Fragment>
     );
 };
 
