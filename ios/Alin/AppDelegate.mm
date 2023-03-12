fa5008a3c50f4c9f49d10f4e1a2f93490a3759b8
Yevhen Volynets <yvolynets@exoft.net>
2023-03-12 18:07:29 +0200

    Updated node version, fixed build issues

---
+++ ios/Alin/AppDelegate.mm
@@ -0,0 +1,25 @@
+#import "AppDelegate.h"
+#import <React/RCTBundleURLProvider.h>
+
+@implementation AppDelegate
+  - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  self.moduleName = @"main";
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+}
+
+- (BOOL)concurrentRootEnabled
+{
+  return true;
+}
+
+@end
